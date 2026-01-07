e61775a37b7e1211c5efd9070be5924d0ad638f9
Bl00D4NGEL <github@d-peters.com>
2026-01-07 22:41:01 +0100

    match: func_8005CAD0_5D6D0

--- src/58080.c
+++ src/58080.c
@@ -1,4 +1,55 @@
 #include "common.h"
+#include "game/data.h"
+
+typedef struct {
+    u16 pad_0;
+    u8 unk_2;
+    u8 pad_3;
+    u8 unk_4;
+    u8 pad_5[0xA-5];
+    s16 unk_A;
+    s16 *unk_C;
+    u8 pad_10[0x20-0x10];
+    void *unk_20;
+    void *unk_24;
+} BoardPlayer;
+
+BoardPlayer* BoardPlayerGet(s32);
+s16 SprAnimRead(void*);
+s32 SprAnimSet(s16, s16, s16, s32);
+s32 SprPosSet(s16, s16, s16, s16);
+s32 SprPrioSet(s16, s16, s32);
+s32 SprScaleSet(s16, s32, f32, f32);
+void SprAttrReset(s16, s16, u16);
+void SprAttrSet(s16, s16, u32);
+s32 _CheckFlag(s32);
+s32 func_8005B518_5C118(s16, s32);
+s32 func_8005C6DC_5D2DC(BoardPlayer*, s16);
+s32 func_8005C78C_5D38C(BoardPlayer*, s32);
+s32 func_8005C8A0_5D4A0(s32);
+s32 func_8005C8AC_5D4AC(void*, s16);
+void* func_8005C980_5D580(void);
+s32 func_8005DCA0_5E8A0(s16);
+s32 func_8005DEB0_5EAB0(s16, u8);
+BoardPlayer *func_8005E688_5F288(s32, s32);
+s32 func_8005E6E4_5F2E4(BoardPlayer*);
+s32 func_8007959C_7A19C(s32);
+s32 func_80082450_83050(s16, s16, s32, s32, s32);
+s32 func_800824B4_830B4(s16, s32, s32, s32, s32, s32);
+void func_8004430C_44F0C(void*);
+
+extern s32 D_800CC754_CD354[6];
+extern s32 D_800CC76C_CD36C[6];
+extern s32 D_800CC784_CD384[8];
+extern s8 D_800CC7A4_CD3A4[8];
+extern s8 D_800CC7AC_CD3AC[8];
+extern s16 D_800CC7B4_CD3B4[8];
+extern s16 D_800CC7BC_CD3BC[8];
+extern u16 D_800CC7C4_CD3C4[22][1];
+extern s16 *D_800E1CF0_E28F0;
+extern u16 D_800F6098_F6C98[]; // unknown size
+extern u16 D_8010163C_10223C[]; // unknown size
+extern GW_PLAYER gPlayers[];
 
 INCLUDE_ASM(const s32, "58080", func_80057480_58080);
 
@@ -251,7 +302,231 @@ INCLUDE_ASM(const s32, "58080", func_8005C8AC_5D4AC);
 
 INCLUDE_ASM(const s32, "58080", func_8005C980_5D580);
 
-INCLUDE_ASM(const s32, "58080", func_8005CAD0_5D6D0);
+s32 func_8005CAD0_5D6D0(s32 arg0) {
+    u16 sp1E;
+    BoardPlayer *boardPlayer1;
+    BoardPlayer *boardPlayer2;
+    BoardPlayer* boardPlayer3;
+    BoardPlayer* boardPlayer4;
+    BoardPlayer* tempBoardPlayer;
+    Process* process;
+    s32 sp3C;
+    s16 var_a0_2;
+    s32 i;
+    s32 var_v0;
+    s32 var_s1;
+    void** mainFSData;
+
+    sp3C = 0;
+    sp1E = 0;
+    for (i = 0; i < 4; i++) {
+        if (func_8005DCA0_5E8A0(i) == 0 && gPlayers[i].port == arg0) {
+            sp1E = i;
+            break;
+        }
+    }
+    boardPlayer1 = func_8005E688_5F288(8, 0);
+    for (i = 0; i < 4; i++) {
+        if (i == 0) {
+            mainFSData = DataRead(0xA01C8);
+            boardPlayer1->unk_C[0] = SprAnimRead(mainFSData);
+            DataClose(mainFSData);
+            mainFSData = DataRead(0xA01C9);
+            boardPlayer1->unk_C[4] = SprAnimRead(mainFSData);
+            DataClose(mainFSData);
+        }
+        SprAnimSet(boardPlayer1->unk_A, i, boardPlayer1->unk_C[0], 0);
+        SprPrioSet(boardPlayer1->unk_A, i, 8);
+        SprAttrSet(boardPlayer1->unk_A, i, 0x1000U);
+        SprPosSet(boardPlayer1->unk_A, i, D_800CC7B4_CD3B4[i], D_800CC7BC_CD3BC[i]);
+        func_80082450_83050(boardPlayer1->unk_A, i, 0, 0, 0xFF);
+        SprAnimSet(boardPlayer1->unk_A, i + 4, boardPlayer1->unk_C[4], 0);
+        SprPrioSet(boardPlayer1->unk_A, i + 4, 8);
+        SprAttrSet(boardPlayer1->unk_A, i + 4, 0x1000U);
+        SprPosSet(boardPlayer1->unk_A, i + 4, D_800CC7B4_CD3B4[i], D_800CC7BC_CD3BC[i]);
+    }
+    boardPlayer2 = func_8005E688_5F288(8, 0);
+    for (i = 0; i < 4; i++) {
+        tempBoardPlayer = BoardPlayerGet(i);
+        mainFSData = DataRead(D_800CC754_CD354[tempBoardPlayer->unk_4]);
+        boardPlayer2->unk_C[i] = SprAnimRead(mainFSData);
+        SprAnimSet(boardPlayer2->unk_A, i, boardPlayer2->unk_C[i], 0);
+        SprPrioSet(boardPlayer2->unk_A, i, 7);
+        SprAttrSet(boardPlayer2->unk_A, i, 0x1000U);
+        SprPosSet(boardPlayer2->unk_A, i, D_800CC7B4_CD3B4[i], D_800CC7BC_CD3BC[i] - 0x17);
+        DataClose(mainFSData);
+        mainFSData = DataRead(D_800CC76C_CD36C[tempBoardPlayer->unk_4]);
+        boardPlayer2->unk_C[i + 4] = SprAnimRead(mainFSData);
+        SprAnimSet(boardPlayer2->unk_A, i + 4, boardPlayer2->unk_C[i + 4], 0);
+        SprPrioSet(boardPlayer2->unk_A, i + 4, 7);
+        SprAttrSet(boardPlayer2->unk_A, i + 4, 0x1000U);
+        SprPosSet(boardPlayer2->unk_A, i + 4, D_800CC7B4_CD3B4[i], D_800CC7BC_CD3BC[i] - 7);
+        DataClose(mainFSData);
+    }
+    boardPlayer3 = func_8005E688_5F288(8, 0);
+    for (i = 0; i < 8; i++) {
+        mainFSData = DataRead(D_800CC784_CD384[i]);
+        boardPlayer3->unk_C[i] = SprAnimRead(mainFSData);
+        DataClose(mainFSData);
+    }
+    for (i = 0; i < 4; i++) {
+        if (func_8005DCA0_5E8A0(i) != 0) {
+            SprAnimSet(boardPlayer3->unk_A, i, boardPlayer3->unk_C[D_800CC7AC_CD3AC[gPlayers[i].cpu_difficulty2 * 2]], 0);
+            SprAnimSet(boardPlayer3->unk_A, (i + 4), boardPlayer3->unk_C[D_800CC7AC_CD3AC[(gPlayers[i].cpu_difficulty2 * 2) + 1]], 0);
+            SprPosSet(boardPlayer3->unk_A, i, D_800CC7B4_CD3B4[i], D_800CC7BC_CD3BC[i] + 8);
+        } else {
+            SprAnimSet(boardPlayer3->unk_A, i, boardPlayer3->unk_C[D_800CC7A4_CD3A4[gPlayers[i].port * 2]], 0);
+            SprAnimSet(boardPlayer3->unk_A, i + 4, boardPlayer3->unk_C[D_800CC7A4_CD3A4[gPlayers[i].port * 2]], 0);
+            SprPosSet(boardPlayer3->unk_A, i, D_800CC7B4_CD3B4[i], D_800CC7BC_CD3BC[i] + 0x10);
+            SprAttrSet(boardPlayer3->unk_A, i + 4, 0x8000U);
+        }
+        SprPrioSet(boardPlayer3->unk_A, i, 7);
+        SprAttrSet(boardPlayer3->unk_A, i, 0x01001000U);
+        SprPrioSet(boardPlayer3->unk_A, i + 4, 7);
+        SprAttrSet(boardPlayer3->unk_A, i + 4, 0x01001000U);
+        SprPosSet(boardPlayer3->unk_A, i + 4, D_800CC7B4_CD3B4[i], D_800CC7BC_CD3BC[i] + 0x18);
+    }
+    boardPlayer4 = func_8005E688_5F288(3, 0);
+    mainFSData = DataRead(0xA001B);
+    boardPlayer4->unk_C[0] = SprAnimRead(mainFSData);
+    SprAnimSet(boardPlayer4->unk_A, 0, boardPlayer4->unk_C[0], 0);
+    SprAttrSet(boardPlayer4->unk_A, 0, 0x1808U);
+    SprPrioSet(boardPlayer4->unk_A, 0, 8);
+    SprScaleSet(boardPlayer4->unk_A, 0, 0.5f, 0.5f);
+    DataClose(mainFSData);
+    mainFSData = DataRead(0xA01DF);
+    boardPlayer4->unk_C[1] = SprAnimRead(mainFSData);
+    SprAnimSet(boardPlayer4->unk_A, 1, boardPlayer4->unk_C[1], 0);
+    SprAttrSet(boardPlayer4->unk_A, 1, 0x9808U);
+    SprPrioSet(boardPlayer4->unk_A, 1, 8);
+    func_800824B4_830B4(boardPlayer4->unk_A, 1, 0xFF, 0, 0, 0x80);
+    DataClose(mainFSData);
+    mainFSData = DataRead(0xA01DE);
+    boardPlayer4->unk_C[2] = SprAnimRead(mainFSData);
+    SprAnimSet(boardPlayer4->unk_A, 2, boardPlayer4->unk_C[2], 0);
+    SprAttrSet(boardPlayer4->unk_A, 2, 0x9808U);
+    SprPrioSet(boardPlayer4->unk_A, 2, 8);
+    func_800824B4_830B4(boardPlayer4->unk_A, 2, 0xFF, 0, 0, 0x80);
+    DataClose(mainFSData);
+    func_8005C8AC_5D4AC(boardPlayer4, sp1E);
+    func_8005C8A0_5D4A0(0);
+    process = omAddPrcObj(func_8005C980_5D580, 0x1005U, 0, 0);
+    process->user_data = boardPlayer4;
+    do {
+        _CheckFlag(0xD);
+        if (sp3C != 0x51B) {
+            sp3C = 0x51B;
+            func_8005B518_5C118(D_800E1CF0_E28F0[4], sp3C);
+        }
+        HuPrcVSleep();
+        var_a0_2 = -1;
+        if (D_800F6098_F6C98[arg0] & 0x800) {
+            var_a0_2 = 0;
+        }
+        if (D_800F6098_F6C98[arg0] & 0x400) {
+            var_a0_2 = 1;
+        }
+        if (D_800F6098_F6C98[arg0] & 0x200) {
+            var_a0_2 = 2;
+        }
+        if (D_800F6098_F6C98[arg0] & 0x100) {
+            var_a0_2 = 3;
+        }
+        if (var_a0_2 != -1) {
+            sp1E += D_800CC7C4_CD3C4[(s16)sp1E << 2][var_a0_2];
+            func_8005C8AC_5D4AC(boardPlayer4, sp1E);
+            func_8007959C_7A19C(1);
+            continue;
+        }
+
+        // wtf? if 0-ed to keep code in "correct" place
+        if (0) {
+            dummy:
+            func_8007959C_7A19C(2);
+            func_8005DEB0_5EAB0(sp1E, var_s1);
+            goto bail;
+        }
+        if (D_8010163C_10223C[arg0] & 0x8000) {
+            func_8007959C_7A19C(2);
+            tempBoardPlayer = BoardPlayerGet((s16)sp1E);
+            func_8005C8A0_5D4A0(1);
+            var_s1 = tempBoardPlayer->unk_2;
+            i = -1;
+            if (func_8005DCA0_5E8A0(sp1E)) {
+                i = 4;
+            }
+            while (1) {
+                HuPrcVSleep();
+                _CheckFlag(0xD);
+                if (sp3C != 0x51C) {
+                    sp3C = 0x51C;
+                    func_8005B518_5C118(D_800E1CF0_E28F0[4], sp3C);
+                }
+                if (D_800F6098_F6C98[arg0] & 0x200) {
+                    if (i != 4 || var_s1 == 0) {
+                        i = func_8005C78C_5D38C(tempBoardPlayer, -1);
+                        if (i == 4) {
+                            var_s1 = 2;
+                        }
+                    } else {
+                        var_s1 -= 1;
+                    }
+                    func_8005DEB0_5EAB0(sp1E, var_s1);
+                    func_8007959C_7A19C(1);
+                } else if (D_800F6098_F6C98[arg0] & 0x100) {
+                    if (i != 4 || var_s1 == 2) {
+                        i = func_8005C78C_5D38C(tempBoardPlayer, 1);
+                        if (i == 4) {
+                            var_s1 = 0;
+                        } else {
+                            var_s1 &= -1;
+                        }
+                    } else {
+                        var_s1 += 1;
+                    }
+                    func_8005DEB0_5EAB0(sp1E, var_s1);
+                    func_8007959C_7A19C(1);
+                }
+                if (i >= 0) {
+                    func_8005C6DC_5D2DC(tempBoardPlayer, i);
+                    if (func_8005DCA0_5E8A0(sp1E) != 0) {
+                        SprAnimSet(boardPlayer3->unk_A, sp1E, boardPlayer3->unk_C[D_800CC7AC_CD3AC[gPlayers[(s16)sp1E].cpu_difficulty2 * 2]], 0);
+                        SprAnimSet(boardPlayer3->unk_A, sp1E + 4, boardPlayer3->unk_C[D_800CC7AC_CD3AC[(gPlayers[(s16)sp1E].cpu_difficulty2) * 2 + 1]], 0);
+                        SprPosSet(boardPlayer3->unk_A, sp1E, D_800CC7B4_CD3B4[(s16)sp1E], D_800CC7BC_CD3BC[(s16)sp1E] + 8);
+                        SprAttrReset(boardPlayer3->unk_A, sp1E + 4, 0x8000U);
+                    } else {
+                        SprAnimSet(boardPlayer3->unk_A, sp1E, boardPlayer3->unk_C[D_800CC7A4_CD3A4[gPlayers[(s16)sp1E].port * 2]], 0);
+                        SprPosSet(boardPlayer3->unk_A, sp1E, D_800CC7B4_CD3B4[(s16)sp1E], D_800CC7BC_CD3BC[(s16)sp1E] + 0x10);
+                        SprAttrSet(boardPlayer3->unk_A, sp1E + 4, 0x8000U);
+                    }
+                }
+                var_v0 = D_800F6098_F6C98[arg0];
+                var_v0 &= 0x8000;
+                if (var_v0) {
+                    goto dummy;
+                }
+
+                var_v0 = D_800F6098_F6C98[arg0];
+                var_v0 &= 0x4000;
+                if (var_v0) {
+                    func_8007959C_7A19C(4);
+                    break;
+                }
+            }
+            
+            bail:
+            func_8005C8A0_5D4A0(0);
+            HuPrcVSleep();
+        }
+    } while (!(D_8010163C_10223C[arg0] & 0x4000));
+    func_8007959C_7A19C(4);
+    omDelPrcObj(process);
+    func_8005E6E4_5F2E4(boardPlayer4);
+    func_8005E6E4_5F2E4(boardPlayer3);
+    func_8005E6E4_5F2E4(boardPlayer2);
+    func_8005E6E4_5F2E4(boardPlayer1);
+    return 4;
+}
 
 INCLUDE_ASM(const s32, "58080", func_8005D7D4_5E3D4);
 
@@ -295,10 +570,8 @@ INCLUDE_ASM(const s32, "58080", func_8005E1DC_5EDDC);
 
 INCLUDE_ASM(const s32, "58080", func_8005E228_5EE28);
 
-GW_PLAYER* BoardPlayerGet(s32);
-
 void func_8005E260_5EE60(s16 playerIndex) {
-    GW_PLAYER* temp_v0;
+    BoardPlayer* temp_v0;
 
     temp_v0 = BoardPlayerGet(playerIndex);
     if (temp_v0->unk_24 != 0) {
